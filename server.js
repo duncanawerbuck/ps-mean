db86ed793e561ca0a84bafeca2dd0463552cff17
githubUser919 <duk444@gmail.com>
2014-05-15 15:28:10 +0100

    added express app

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-//TODO: Continue with "Creating the Node Application" 01:19
+app.configure(function () {
+    app.set('views', __dirname + '/server/views');
+    app.set('view engine', 'jase');
+});
+
+app.get('*', function (req, res) {
+    res.render('index'); //TODO: Create view ("Creating the Node Application" @ 04:55)
+});
