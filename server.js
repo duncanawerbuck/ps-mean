f46947db471551e34d9ae77f5d28902fa6e492ac
githubUser919 <duk444@gmail.com>
2014-05-15 16:37:44 +0100

    Express app serves up Hello World.

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ var app = express();
 
 app.configure(function () {
     app.set('views', __dirname + '/server/views');
-    app.set('view engine', 'jase');
+    app.set('view engine', 'jade');
 });
 
 app.get('*', function (req, res) {
-    res.render('index'); //TODO: Create view ("Creating the Node Application" @ 04:55)
+    res.render('index');
 });
+
+var port = 3030;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
