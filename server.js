7765d232eb0e7f5773b9f4aacafcf023f8daac52
githubUser919 <duk444@gmail.com>
2014-05-15 15:09:20 +0100

    +server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+//TODO: Continue with "Creating the Node Application" 01:19
