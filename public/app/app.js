857dfdb2c391dd587b11507cba57f346ea34e3b2
githubUser919 <duk444@gmail.com>
2014-05-16 12:50:00 +0100

    Angular working, fixed class name in index.jade

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,12 @@
-//TODO: Continue with "Creating the Angular Application", 00:30
+angular.module('app', ['ngResource', 'ngRoute']);
+
+angular.module('app').config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+        .when('/', { templateUrl: '/partials/main', controller: 'mainCtrl' })
+    });
+
+// mainCtrl
+angular.module('app').controller('mainCtrl', function ($scope) {
+    $scope.myVar = "Hello Angular";
+});
